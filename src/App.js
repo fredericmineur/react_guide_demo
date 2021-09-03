df99358c8e8f90ee8d0b984682833a61d3f7ecf1
Frederic Mineur <fredericmineur@gmail.com>
2021-09-03 19:00:48 +0100

    Alternative to JSX with React.createElement()

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1>Hi, I'm a React App</h1>
+    //   </div>
+    // );
+
+    //nesting elements... 
+    // React.createElement(component, props, ...children)
+    return React.createElement('div', {className: 'App'}, 
+      React.createElement('h1', null, 'Hi, I\'m a React App!'));
   }
 }
 

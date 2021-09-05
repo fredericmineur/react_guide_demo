39564a54db6132fa4979acf3fe2cd27772ea61f5
Frederic Mineur <fredericmineur@gmail.com>
2021-09-05 22:15:44 +0100

    convert class component to functional component; demo useState hook

--- src/App.js
+++ src/App.js
@@ -1,46 +1,50 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
 
-class App extends Component {
 
-  // We are in a class, so no declaration with 'const'
-  // 'state' is a compulsory name for the property (hence linked to setState)
-    state = {
-    persons :[
-      {name: 'Max', age:23},
-      {name: 'Egar', age:47},
-      {name: 'Marie', age:64}  
-    ],
-    otherState: 'another value'
+
+//functional component
+const app = () => {
+
+  //declaring properties with const
+  //useState hook: const [state, setState] = useState(initialState)
+  // setState is the state mutator
+  const [personState, setPersonState] = useState({
+    persons: [
+      { name: 'Max', age: 23 },
+      { name: 'Egar', age: 47 },
+      { name: 'Marie', age: 64 }
+    ]
+  })
+
+  const [otherState, setOtherState] = useState('another value');
+
+  const switchHandler = () => {
+    setPersonState({
+      persons: [
+        { name: 'Max', age: 25 },
+        { name: 'Egar', age: 49 },
+        { name: 'Marie', age: 66 }
+      ]
+    });
   }
 
-  switchHandler = () => {
-    // it overwrites the original state ...only persons...otherState is still in the same state
-    this.setState({
-      persons:[
-      {name: 'Max', age:25},
-      {name: 'Egar', age:49},
-      {name: 'Marie', age:66}
-    ]});
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        {/*!!! onClick is JSX...not HTML onclick event*/}
-        <button onClick={this.switchHandler}>Toggle</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>
-          {/* children inside the component*/}
-          My hobby is reptile keeping</Person>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-          <p>{this.state.otherState}</p>
-      </div>
-    );
-  }
+  // no more render() {} ...just return(JSX)
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      {/*!!! onClick is JSX...not HTML onclick event*/}
+      <button onClick={switchHandler}>Toggle</button>
+      <Person name={personState.persons[0].name} age={personState.persons[0].age}>
+        {/* children inside the component*/}
+        My hobby is reptile keeping</Person>
+      <Person name={personState.persons[1].name} age={personState.persons[1].age} />
+      <Person name={personState.persons[2].name} age={personState.persons[2].age} />
+      <p>{otherState}</p>
+    </div>
+  );
 }
 
-export default App;
+export default app;
